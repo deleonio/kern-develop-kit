f391846af396ffdf8f0933193a9c31993e7b8d9b
Axel Wolters <axel.wolters@sk.hamburg.de>
2025-10-20 10:51:33 +0200

    chore: update package.json formatting and dependencies
    - Reformatted package.json for consistent indentation.
    - Added prettier overrides for JSON files to use 2 spaces instead of tabs.
    - Updated stylelint-rules package.json to use consistent formatting.
    - Reformatted tsconfig.json for consistent indentation.

--- prettier.config.js
+++ prettier.config.js
@@ -2,4 +2,13 @@ export default {
 	printWidth: 160,
 	singleQuote: true,
 	useTabs: true,
+	overrides: [
+		{
+			files: ['*.json', '*.jsonc'],
+			options: {
+				useTabs: false,
+				tabWidth: 2,
+			},
+		},
+	],
 };
